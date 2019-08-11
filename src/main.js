9caec48ca4a0ef5f901018214917d16fab7c459d
Eugene Lanets <mrShaftoe@gmail.com>
2019-08-11 13:11:19 +0300

    menu and filters added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import {getRoute} from './components/route';
+import {getMenu} from './components/menu';
+import {getFilters} from './components/filters';
 
 const renderComponent = function (container, content, position) {
   container.insertAdjacentHTML(position, content);
 };
 
 const routeContainer = document.querySelector(`.trip-info`);
+const [menuContainer, filtersContainer] = document.querySelectorAll(`.trip-controls h2`);
 
 renderComponent(routeContainer, getRoute(), `afterbegin`);
+renderComponent(menuContainer, getMenu(), `afterend`);
+renderComponent(filtersContainer, getFilters(), `afterend`);
